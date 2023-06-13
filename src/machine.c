98a22eabf503a5fbcc72df4c4de31150d53b9572
eren662003 <e.celikel2003@hotmail.com>
2023-06-13 16:50:57 +0200

    task 1 finished

--- src/machine.c
+++ src/machine.c
@@ -20,35 +20,106 @@ void set_output(FILE *fp)
   out = fp; 
 }
 
+typedef struct
+  {
+    uint32_t magic_num;
+    uint32_t constant_ori;
+    uint32_t constant_siz;
+    uint32_t txt_size;
+    uint32_t txt_ori;
+    int32_t *constant_array;
+    uint8_t *text_array;
+  }  binary_data;
+
+binary_data binary_dataa;
+
 int init_ijvm(char *binary_path) 
 {
+  puts("test");
   in = stdin;
   out = stdout;
+
+  FILE  *f = fopen(binary_path, "rb");
+  uint8_t buff1[4];
+  uint8_t buff2;
+
+  fread(&buff1, sizeof(uint8_t), 4, f);
+  uint32_t magic_number = read_uint32_t(buff1);
+  binary_dataa.magic_num = magic_number;
+  printf("magic size is %d", magic_number);
+
+  fread(&buff1, sizeof(uint8_t), 4, f);
+  uint32_t constant_origin = read_uint32_t(buff1);
+  binary_dataa.constant_ori = constant_origin;
+
+  fread(&buff1, sizeof(uint8_t), 4, f);
+  uint32_t constant_size = read_uint32_t(buff1);
+  binary_dataa.constant_siz = constant_size;
+
+  binary_dataa.constant_array = (int32_t*) malloc(constant_size * sizeof(int32_t));
+
+  for (int i = 0; i < constant_size/4; i++){
+      fread(&buff1, sizeof(uint8_t), 4, f);
+      binary_dataa.constant_array[i] = read_uint32_t(buff1);
+      fprintf(stderr, "constant is nu : %d\n", binary_dataa.constant_array[i]);
+  }
+
+  fread(&buff1, sizeof(uint8_t), 4, f);
+  uint32_t text_origin = read_uint32_t(buff1);
+  binary_dataa.txt_ori = text_origin;
+
+
+  fread(&buff1, sizeof(uint8_t), 4, f);
+  uint32_t text_size = read_uint32_t(buff1);
+  binary_dataa.txt_size = text_size;
+  fprintf(stderr, "the text szie is : %d\n", text_size);
+
+  binary_dataa.text_array = (uint8_t*) malloc(text_size * sizeof(uint8_t));
+
+  for (int i = 0; i < text_size; i++){
+      fread(&buff2, sizeof(uint8_t), 1, f);
+      fprintf(stderr, "%x\n", buff2);
+      binary_dataa.text_array[i] = buff2;
+  }
+
+
+
+
+// loop constant size/4 in een dynamic array met malloc 
+// constant is int32     text uint8
+  // fread(&buff1, sizeof(uint8_t), binary_dataa.constant_siz, f);
+  // uint32_t constant_data = read_uint32_t(&buff1);
+  // // binary_dataa.constant_dat = constant_data;
+
+
+
+  // fread(&buff1, sizeof(uint8_t), constant_size, f);
+
+  
+
   // TODO: implement me
-  return -1;
+  return 0;
 }
 
 void destroy_ijvm(void) 
 {
-  // TODO: implement me
+  free (binary_dataa.constant_array);
+  free (binary_dataa.text_array);
 }
 
 byte_t *get_text(void) 
 {
-  // TODO: implement me
-  return NULL;
+  return binary_dataa.text_array;
 }
 
 unsigned int get_text_size(void) 
 {
-  // TODO: implement me
-  return 0;
+  return binary_dataa.txt_size;
 }
 
 word_t get_constant(int i) 
 {
-  // TODO: implement me
-  return 0;
+  return binary_dataa.constant_array[i];
 }
 
 unsigned int get_program_counter(void) 
